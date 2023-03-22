2d4ecef119bcc55ccd3f7e8ec7d8a9d1a9c6e4bc
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2023-03-22 11:18:11 -0300

    create global css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,9 @@
+import { styled } from "../styles";
+
+const Button = styled("button", {
+  backgroundColor: "$green500",
+});
+
 export default function Home() {
-  return <h1>Hello World!</h1>;
+  return <Button>Enviar</Button>;
 }
