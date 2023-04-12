1f91e5b85376c966bcf3d978409d4f7bc62c5719
Willames Santos <willamessantos@live.com>
2023-04-11 23:09:45 -0300

    connection api strapi

---
+++ src/lib/stripe.ts
@@ -0,0 +1,8 @@
+import Stripe from "stripe";
+
+export const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY, {
+  apiVersion: "2022-11-15",
+  appInfo: {
+    name: "Bom café shop",
+  },
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+import { GetServerSideProps } from "next";
 import Image from "next/image";
-import { HomeContainer, Product } from "../styles/pages/home";
 import { useKeenSlider } from "keen-slider/react";
 
+import { stripe } from "../lib/stripe";
+import Stripe from "stripe";
+
+import { HomeContainer, Product } from "../styles/pages/home";
 import "keen-slider/keen-slider.min.css";
 
 import coffee1 from "../assets/1.jpg";
@@ -9,7 +13,16 @@ import coffee2 from "../assets/2.jpg";
 import coffee3 from "../assets/3.jpg";
 import coffee4 from "../assets/4.jpg";
 
-export default function Home() {
+interface HomeProps {
+  products: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: number;
+  }[];
+}
+
+export default function Home({ products }: HomeProps) {
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3,
@@ -18,37 +31,39 @@ export default function Home() {
   });
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
-      <Product className="keen-slider__slide">
-        <Image src={coffee1} width={520} height={480} alt="" />
-        <footer>
-          <strong>Café Arábica</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image src={coffee2} width={520} height={480} alt="" />
-        <footer>
-          <strong>Café Arábica</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image src={coffee3} width={520} height={480} alt="" />
-        <footer>
-          <strong>Café Arábica</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image src={coffee4} width={520} height={480} alt="" />
-        <footer>
-          <strong>Café Arábica</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
+      {products.map((product) => {
+        return (
+          <Product key={product.id} className="keen-slider__slide">
+            <Image src={product.imageUrl} width={520} height={480} alt="" />
+            <footer>
+              <strong>{product.name}</strong>
+              <span>R$ {product.price}</span>
+            </footer>
+          </Product>
+        );
+      })}
     </HomeContainer>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
+
+  const products = response.data.map((product) => {
+    const price = product.default_price as Stripe.Price;
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: price.unit_amount / 100,
+    };
+  });
+
+  return {
+    props: {
+      products,
+    },
+  };
+};
