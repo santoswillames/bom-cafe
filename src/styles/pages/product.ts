52d4eef3d11d2c218a53aaf303603532a0880848
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2023-04-13 15:41:50 -0300

    create product page

--- src/styles/pages/product.ts
+++ src/styles/pages/product.ts
@@ -1,7 +1,68 @@
 import { styled } from "..";
 
-export const ProductContainer = styled("main", {});
+export const ProductContainer = styled("main", {
+  display: "grid",
+  gridTemplateColumns: "1fr 1fr",
+  alignItems: "stretch",
+  gap: "4rem",
+  maxWidth: 1180,
+  margin: "0 auto",
+});
 
-export const ImageContainer = styled("div", {});
+export const ImageContainer = styled("div", {
+  width: 520,
+  maxWidth: 576,
+  height: 480,
+  //   height: "calc(656px - 0.5rem)",
+  background: "$gray800",
+  borderRadius: 8,
+  //   padding: "0.25rem",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  overflow: "hidden",
 
-export const ProductDetails = styled("div", {});
+  img: {
+    objectFit: "cover",
+  },
+});
+
+export const ProductDetails = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+
+  h1: {
+    fontSize: "$2xl",
+    color: "$gray300",
+  },
+
+  span: {
+    marginTop: "1rem",
+    display: "block",
+    fontSize: "$2xl",
+    color: "$orange500",
+  },
+
+  p: {
+    marginTop: "2.5rem",
+    fontSize: "$md",
+    lineHeight: 1.6,
+    color: "$gray300",
+  },
+
+  button: {
+    marginTop: "auto",
+    backgroundColor: "$orange500",
+    border: 0,
+    color: "$white",
+    borderRadius: 8,
+    padding: "1.25rem",
+    cursor: "pointer",
+    fontWeight: "bold",
+    fontSize: "$md",
+
+    "&:hover": {
+      backgroundColor: "$orange300",
+    },
+  },
+});
